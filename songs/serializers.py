674f8591c9eec535f1fc9bdd8963bf48498eb1e3
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2023-01-02 08:21:34 -0300

    entrega

--- songs/serializers.py
+++ songs/serializers.py
@@ -3,11 +3,8 @@ from rest_framework import serializers
 from .models import Song
 
 
-class SongSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255)
-    duration = serializers.CharField(max_length=255)
-    album_id = serializers.IntegerField(read_only=True)
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Song
 
-    def create(self, validated_data):
-        return Song.objects.create(**validated_data)
+        fields = ["id", "title", "duration", "album_id"]
